a01a41f62be66e4bf72138725912c5734378ef8a
pdlmn <djeinwings@gmail.com>
2021-11-07 23:27:14 +0200

    Refactored the code

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,123 +1,132 @@
-const settings = (function() {
+const DOM = (function() {
   const markSelectionDiv = document.querySelector('[id="mark-select"]');
   const gameStartButton = document.querySelector('#game-start');
-  let gameStarted = false;
-  let selectedMark;
-  let player;
-  let computer;
+  const board = document.querySelector('#gameboard');
 
-  init();
+  return { markSelectionDiv, gameStartButton, board }
+})();
 
-  function changeSelectedMark() {
-    selectedMark = document.querySelector('input[name="mark-select"]:checked').value;
-  }
-
+const players = (function() {
   function Player(name, mark) {
     let theirTurn = (mark === 'x');
-    function startTurn() {
-      this.theirTurn = true;
-    }
-    function endTurn() {
-      this.theirTurn = false;
-    }
-    return { name, mark, theirTurn, startTurn, endTurn };
+    return { name, mark, theirTurn };
   }
 
-  function startGame() {
-    gameStarted = true;
-    player = Player('Player', 'x');
-    computer = Player('Computer', 'o');
-    console.log(player)
+  let playerOne = Player('Player one', 'x');
+  let playerTwo = Player('Player two', 'o');
+
+  function changeMark(newMark) {
+    if (newMark === 'x') {
+      [this.playerOne.mark, this.playerTwo.mark] = ['x', 'o'];
+      [this.playerOne.theirTurn, this.playerTwo.theirTurn] = [true, false];
+    } 
+    if (newMark === 'o') {
+      [this.playerOne.mark, this.playerTwo.mark] = ['o', 'x'];
+      [this.playerOne.theirTurn, this.playerTwo.theirTurn] = [false, true];
+    }
   }
 
+  function turnHandler() {
+    let turnOf = this.playerOne.theirTurn ? this.playerOne : this.playerTwo;
+    this.playerOne.theirTurn = !this.playerOne.theirTurn;
+    this.playerTwo.theirTurn = !this.playerTwo.theirTurn;
+    console.log(playerOne, playerTwo)
+    return turnOf.mark;
+  }
+
+  return { playerOne, playerTwo, changeMark, turnHandler }
+})();
+
+const gameBoard = (function() {
+  let boardState = [
+    0, 0, 0,
+    0, 0, 0,
+    0, 0, 0
+  ]
+
+  function render() {
+    DOM.board.replaceChildren();
+    for (let i = 0; i < 9; i++) {
+      let cell = document.createElement('div');
+      cell.setAttribute('data-index', i);
+      cell.classList.add('cell');
+      if (boardState[i] === 'x' || boardState[i] === 'o') {
+        cell.textContent = boardState[i];
+      }
+      DOM.board.appendChild(cell);
+    }
+  }
+
+  function placeMark(e) {
+    let index = e.target.dataset.index;
+    let nextMark = players.turnHandler();
+    boardState[index] = nextMark;
+    console.log(boardState);
+    render();
+  }
+
+  function checkForWin(player) {
+    let mark = player.mark;
+    for (let i = 0; i <= 6; i += 3) {
+      if (boardState[i] === mark && boardState[i + 1] === mark && boardState[i + 2] === mark) {
+        alert(`${player.name} won!`);
+      }
+    }
+
+    for (let i = 0; i <= 2; i++) {
+      if (boardState[i] === mark && boardState[i + 3] === mark && boardState[i + 6] === mark) {
+        alert(`${player.name} won!`);
+      }
+    } 
+
+    let i = 4;
+    if (boardState[i] === mark && boardState[i + 4] === mark && boardState[i - 4] === mark ||
+        boardState[i] === mark && boardState[i + 2] === mark && boardState[i - 2] === mark) {
+      alert(`${player.name} won!`);
+    }
+  }
+
+  function checkForDraw() {
+    if (!boardState.includes(0)) {
+      alert('draw!');
+    }
+  }
+
+  function delegateBoardEvents() {
+    DOM.board.addEventListener('click', e => { 
+      if (e.target.matches('.cell') && !e.target.textContent) {
+        placeMark(e);
+        checkForWin(players.playerOne);
+        checkForWin(players.playerTwo);
+        checkForDraw();
+      }
+    });
+  }
+
+  delegateBoardEvents();
+
+  render();
+})();
+
+const settings = (function() {
   function delegateMarkSelectionEvents() {
-    markSelectionDiv.addEventListener('click', e => {
+    DOM.markSelectionDiv.addEventListener('click', e => {
       if (e.target.matches('input[type="radio"]')) {
-        changeSelectedMark();
+        players.changeMark(e.target.value);
+        //
+        console.log(players.playerOne.mark)
       }
     });
   }
 
   function init() {
     delegateMarkSelectionEvents();
-    gameStartButton.addEventListener('click', startGame);
   }
 
-  return { player, computer }
+  init();
 })();
 
-// contorls game flow
-const gameController = (function({ player, computer }) {
-  const board = document.getElementById('gameboard');
-  let nextMark = 'x';
-  let boardState = [
-    0, 0, 0,
-    0, 0, 0,
-    0, 0, 0
-  ]
-  init();
+const game = (function() {
 
-  function turnHandler() {
-    let turnOf = player.theirTurn ? player : computer;
-    player.theirTurn = !player.theirTurn;
-    computer.theirTurn = !computer.theirTurn;
-    nextMark = turnOf.mark;
-  }
+})();
 
-  function render() {
-    board.replaceChildren();
-    for (let i = 0; i < 9; i++) {
-      let cell = document.createElement('div');
-      cell.setAttribute('data-index', i);
-      cell.classList.add('cell');
-
-
-      if (boardState[i] === 'x' || boardState[i] === 'o') {
-        cell.textContent = boardState[i];
-      }
-      board.appendChild(cell);
-    }
-  }
-
-  function placeMark(e) {
-    let index = e.target.dataset.index;
-    turnHandler();
-    boardState[index] = nextMark;
-    console.log(boardState);
-    render();
-  }
-
-  function delegateBoardEvents() {
-    board.addEventListener('click', e => { 
-      if (e.target.matches('.cell') && !e.target.textContent) {
-        placeMark(e);
-        checkForWin()
-      }
-    });
-  }
-
-  function checkForWin() {
-    for (let i = 0; i <= 6; i += 3) {
-      if (boardState[i] === 'x' && boardState[i + 1] === 'x' && boardState[i + 2] === 'x') {
-        alert('you won!');
-      }
-    }
-
-    for (let i = 0; i <= 2; i++) {
-      if (boardState[i] === 'x' && boardState[i + 3] === 'x' && boardState[i + 6] === 'x') {
-        alert('you won!');
-      }
-    } 
-
-    let i = 4;
-    if (boardState[i] === 'x' && boardState[i + 4] === 'x' && boardState[i - 4] === 'x' ||
-        boardState[i] === 'x' && boardState[i + 2] === 'x' && boardState[i - 2] === 'x') {
-      alert('you won!');
-    }
-  }
-
-  function init() {
-    render();
-    delegateBoardEvents();
-  }
-})(settings);
