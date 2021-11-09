1ba94922978a7bd321745df99b45516e531d9a1e
pdlmn <djeinwings@gmail.com>
2021-11-09 13:57:42 +0200

    Added game module, startGame and and endGame functions, polished other functions

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,9 +1,21 @@
 const DOM = (function() {
-  const markSelectionDiv = document.querySelector('[id="mark-select"]');
-  const gameStartButton = document.querySelector('#game-start');
+  const markSelectionDiv = document.querySelector('#mark-select');
+  const markSelectionInputs = document.querySelectorAll('input[name="mark-select"]');
+  const markInfoDiv = document.querySelector('#mark-info');
+  const startGameButton = document.querySelector('#game-start');
+  const gameStartInfoDiv = document.querySelector('#game-start-info');
+  const winnerInfoDiv = document.querySelector('#winner-info');
   const board = document.querySelector('#gameboard');
 
-  return { markSelectionDiv, gameStartButton, board }
+  return { 
+    markSelectionDiv,
+    markSelectionInputs,
+    markInfoDiv, 
+    startGameButton,
+    gameStartInfoDiv,
+    winnerInfoDiv,
+    board 
+  }
 })();
 
 const players = (function() {
@@ -34,7 +46,12 @@ const players = (function() {
     return turnOf.mark;
   }
 
-  return { playerOne, playerTwo, changeMark, turnHandler }
+  function resetTurns() {
+    this.playerOne.theirTurn = (this.playerOne.mark === 'x');
+    this.playerTwo.theirTurn = (this.playerTwo.mark === 'x');
+  }
+
+  return { playerOne, playerTwo, changeMark, turnHandler, resetTurns }
 })();
 
 const gameBoard = (function() {
@@ -58,37 +75,51 @@ const gameBoard = (function() {
   }
 
   function placeMark(e) {
-    let index = e.target.dataset.index;
-    let nextMark = players.turnHandler();
-    boardState[index] = nextMark;
-    console.log(boardState);
-    render();
+    if (settings.gameStarted === true) {
+      let index = e.target.dataset.index;
+      let nextMark = players.turnHandler();
+      boardState[index] = nextMark;
+      render();
+    }
   }
 
   function checkForWin(player) {
     let mark = player.mark;
     for (let i = 0; i <= 6; i += 3) {
       if (boardState[i] === mark && boardState[i + 1] === mark && boardState[i + 2] === mark) {
-        alert(`${player.name} won!`);
+        DOM.winnerInfoDiv.textContent = `${player.name} won!`;
+        settings.endGame();
       }
     }
 
     for (let i = 0; i <= 2; i++) {
       if (boardState[i] === mark && boardState[i + 3] === mark && boardState[i + 6] === mark) {
-        alert(`${player.name} won!`);
+        DOM.winnerInfoDiv.textContent = `${player.name} won!`;
+        settings.endGame();
       }
     } 
 
     let i = 4;
     if (boardState[i] === mark && boardState[i + 4] === mark && boardState[i - 4] === mark ||
         boardState[i] === mark && boardState[i + 2] === mark && boardState[i - 2] === mark) {
-      alert(`${player.name} won!`);
+          DOM.winnerInfoDiv.textContent = `${player.name} won!`;
+          settings.endGame();
     }
+  }
+
+  function resetBoard() {
+    boardState = [
+      0, 0, 0,
+      0, 0, 0,
+      0, 0, 0
+    ]
+    render();
   }
 
   function checkForDraw() {
     if (!boardState.includes(0)) {
-      alert('draw!');
+      DOM.winnerInfoDiv.textContent = 'Draw!';
+      settings.endGame();
     }
   }
 
@@ -103,30 +134,52 @@ const gameBoard = (function() {
     });
   }
 
-  delegateBoardEvents();
-
-  render();
+  return { delegateBoardEvents, render, resetBoard }
 })();
 
 const settings = (function() {
+  let gameStarted = false;
+
+  function startGame() {
+    this.gameStarted = true;
+    DOM.gameStartInfoDiv.textContent = 'Game has been started!'
+    DOM.gameStartInfoDiv.classList.add('started');
+    DOM.gameStartInfoDiv.classList.remove('not-started');
+    DOM.startGameButton.disabled = true;
+    DOM.startGameButton.textContent = 'Game is ongoing';
+    DOM.winnerInfoDiv.textContent = '';
+    DOM.markSelectionInputs.forEach(input => input.disabled = true)
+    players.resetTurns();
+    gameBoard.resetBoard();
+  }
+
+  function endGame() {
+    this.gameStarted = false;
+    DOM.gameStartInfoDiv.textContent = 'Game ended! Want to restart?'
+    DOM.gameStartInfoDiv.classList.remove('started');
+    DOM.gameStartInfoDiv.classList.add('not-started');
+    DOM.startGameButton.disabled = false;
+    DOM.startGameButton.textContent = 'Restart game';
+    DOM.markSelectionInputs.forEach(input => input.disabled = false)
+  }
+
   function delegateMarkSelectionEvents() {
     DOM.markSelectionDiv.addEventListener('click', e => {
       if (e.target.matches('input[type="radio"]')) {
-        players.changeMark(e.target.value);
-        //
-        console.log(players.playerOne.mark)
+        let mark = e.target.value;
+        players.changeMark(mark);
+        DOM.markInfoDiv.innerHTML = `You chose <span class="">${mark}</span>.`;
       }
     });
   }
 
-  function init() {
-    delegateMarkSelectionEvents();
-  }
-
-  init();
+  return { gameStarted, startGame, endGame, delegateMarkSelectionEvents }
 })();
 
 const game = (function() {
-
+  gameBoard.delegateBoardEvents();
+  gameBoard.render();
+  settings.delegateMarkSelectionEvents();
+  DOM.startGameButton.addEventListener('click', () => settings.startGame());
 })();
 
